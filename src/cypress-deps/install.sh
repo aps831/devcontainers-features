6b1183c9323d6a7ed212f0c121d426f278e5b2cc
Andrew Spratley <aps831@yahoo.co.uk>
2023-01-05 12:33:14 +0000

    feat: add features

---
+++ src/cypress-deps/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+	exit 1
+fi
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+	if ! dpkg -s "$@" >/dev/null 2>&1; then
+		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+			echo "Running apt-get update..."
+			apt-get update -y
+		fi
+		apt-get -y install --no-install-recommends "$@"
+	fi
+}
+
+check_packages libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
---
+++ test/gitmkver/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+source dev-container-features-test-lib
+
+# git-mkver requires a git repository
+git config --global init.defaultBranch master
+git config --global user.email "a@name.com"
+git config --global user.name "A Name"
+
+TMP_DIR=$(mktemp -d)
+cd ${TMP_DIR}
+git init
+touch .gitignore
+git add .gitignore
+git commit -m "Initial commit"
+
+# The 'check' command comes from the dev-container-features-test-lib.
+check "git-mkver info" git-mkver info
+
+reportResults
