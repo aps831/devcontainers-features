c15b8d59c7579b7003d0e3e57f44e3c9ff1a1d8f
Andrew Spratley <aps831@yahoo.co.uk>
2025-01-28 09:45:08 +0000

    test: ensure asdf plugin test fails

--- test/asdf-plugin-manager-asdf/test_defaults.sh
+++ test/asdf-plugin-manager-asdf/test_defaults.sh
@@ -7,6 +7,6 @@ set -e
 source dev-container-features-test-lib
 
 check "asdf-plugin-manager version" asdf-plugin-manager version
-check "asdf plugin list" asdf plugin list
+check "asdf plugin list" asdf plugin list | grep -Pzo "asdf-plugin-manager\nmaven"
 
 reportResults
