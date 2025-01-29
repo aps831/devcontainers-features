05af333bb0387e67d8154486a2516ef1e65cfd88
Andrew Spratley <aps831@yahoo.co.uk>
2025-01-29 12:31:52 +0000

    refactor: update gitleaks

--- src/gitleaks/install.sh
+++ src/gitleaks/install.sh
@@ -1,87 +1,13 @@
 #!/usr/bin/env bash
 
-GITLEAKS_VERSION="${VERSION:-"latest"}"
-
 set -e
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
+source ./library_scripts.sh
 
-architecture="$(uname -m)"
-case ${architecture} in
-x86_64)
-  architecture="x64"
-  ;;
-*)
-  echo "(!) Architecture ${architecture} unsupported"
-  exit 1
-  ;;
-esac
+ensure_nanolayer nanolayer_location "v0.5.5"
 
-if [ "$(id -u)" -ne 0 ]; then
-  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-  exit 1
-fi
-
-# Checks if packages are installed and installs them if not
-check_packages() {
-  if ! dpkg -s "$@" >/dev/null 2>&1; then
-    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-      echo "Running apt-get update..."
-      apt-get update -y
-    fi
-    apt-get -y install --no-install-recommends "$@"
-  fi
-}
-
-# Figure out correct version of a three part version number is not passed
-find_version_from_git_tags() {
-  local variable_name=$1
-  local requested_version=${!variable_name}
-  if [ "${requested_version}" = "none" ]; then return; fi
-  local repository=$2
-  local prefix=${3:-"tags/v"}
-  local separator=${4:-"."}
-  local last_part_optional=${5:-"false"}
-  if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
-    local escaped_separator=${separator//./\\.}
-    local last_part
-    if [ "${last_part_optional}" = "true" ]; then
-      last_part="(${escaped_separator}[0-9]+)?"
-    else
-      last_part="${escaped_separator}[0-9]+"
-    fi
-    local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
-    local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
-    if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
-      declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
-    else
-      set +e
-      declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
-      set -e
-    fi
-  fi
-  if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" >/dev/null 2>&1; then
-    echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
-    exit 1
-  fi
-  echo "${variable_name}=${!variable_name}"
-}
-
-find_version_from_git_tags GITLEAKS_VERSION 'https://github.com/zricethezav/gitleaks'
-
-check_packages curl
-
-GITLEAKS_FILENAME=gitleaks_${GITLEAKS_VERSION}_linux_${architecture}.tar.gz
-
-TMP_DIR=$(mktemp -d)
-cd ${TMP_DIR}
-curl -sSL https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/${GITLEAKS_FILENAME} | tar xvz
-mv gitleaks /usr/local/bin/gitleaks
-chmod +x /usr/local/bin/gitleaks
-
-# Clean up
-rm -rf ${TMP_DIR}
-rm -rf /var/lib/apt/lists/*
-
-echo "Done!"
+$nanolayer_location \
+  install \
+  devcontainer-feature \
+  "ghcr.io/devcontainers-extra/features/gh-release:1.0.25" \
+  --option repo="zricethezav/gitleaks" --option binaryNames="gitleaks" --option version="$VERSION"
