b98336ba93111e3e0ba85f42c355568b33ae7bd7
Andrew Spratley <aps831@yahoo.co.uk>
2025-12-25 22:11:24 +0000

    feat: remove python install inside poetry

--- src/poetry-asdf/install.sh
+++ src/poetry-asdf/install.sh
@@ -13,11 +13,6 @@ ensure_nanolayer nanolayer_location "v0.4.45"
 ${nanolayer_location} \
   install \
   devcontainer-feature \
-  "ghcr.io/aps831/devcontainers-features/python-asdf:1.0.4"
-
-${nanolayer_location} \
-  install \
-  devcontainer-feature \
   "ghcr.io/aps831/devcontainers-features/asdf-plugin-manager-asdf:1.0.2" \
   --option plugin="${PLUGIN}" --option pluginRepo="${PLUGINREPO}" --option ref="${PLUGINREF}"
 
