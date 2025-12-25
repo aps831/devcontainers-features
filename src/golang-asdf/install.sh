41fdb94d30f37d17f155597a99c32187cd5d894d
Andrew Spratley <aps831@yahoo.co.uk>
2025-12-25 21:38:57 +0000

    fix: update location of asdf-golang

--- src/golang-asdf/install.sh
+++ src/golang-asdf/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 PLUGIN="golang"
-PLUGINREPO="https://github.com/kennyp/asdf-golang.git"
-PLUGINREF="e2527a3"
+PLUGINREPO="https://github.com/asdf-community/asdf-golang.git"
+PLUGINREF="7cec918"
 
 set -e
 
@@ -13,7 +13,7 @@ ensure_nanolayer nanolayer_location "v0.4.45"
 ${nanolayer_location} \
   install \
   devcontainer-feature \
-  "ghcr.io/aps831/devcontainers-features/asdf-plugin-manager-asdf:1.0.2" \
+  "ghcr.io/aps831/devcontainers-features/asdf-plugin-manager-asdf:1.0.5" \
   --option plugin="${PLUGIN}" --option pluginRepo="${PLUGINREPO}" --option ref="${PLUGINREF}"
 
 su - "${_REMOTE_USER}" <<EOF
